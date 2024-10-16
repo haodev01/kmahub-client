736d6be9e3326ec8ca5f0dbc99b4d36c7b2cfd25
haonc <haonc@vega.com.vn>
2024-10-16 17:15:04 +0700

    Fix image

--- src/ultis/image.ts
+++ src/ultis/image.ts
@@ -1,15 +1,12 @@
-import { getPlaiceholder } from 'plaiceholder';
-
 export const getBlurImage = async (src: string) => {
-  try {
-    const buffer = await fetch(src).then(async (res) =>
-      Buffer.from(await res.arrayBuffer()),
-    );
-
-    const { base64 } = await getPlaiceholder(buffer);
-
-    return base64;
-  } catch (error) {
-    return error;
-  }
+  // try {
+  //   const buffer = await fetch(src).then(async (res) =>
+  //     Buffer.from(await res.arrayBuffer()),
+  //   );
+  //   const { base64 } = await getPlaiceholder(buffer);
+  //   return base64;
+  // } catch (error) {
+  //   return error;
+  // }
+  return src;
 };
