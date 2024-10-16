f8037de586f98c371c1443d0b0af78f923e5d3d2
haonc <haonc@vega.com.vn>
2024-10-16 08:10:55 +0700

    Fix image

--- src/components/layouts/main/sidebar.tsx
+++ src/components/layouts/main/sidebar.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { SidebarItem } from './sidebar-item';
 
 export const Sidebar = () => {
-  console.log('Test CI CD');
   return (
     <div className="md:sticky fixed bottom-0 w-full md:top-20 md:left-0 md:w-25 md:px-4 md:bg-transparent dark:bg-black bg-white py-1 md:py-0 ">
       <div className="flex items-center justify-center md:flex-col md:gap-x-0 gap-x-6">
